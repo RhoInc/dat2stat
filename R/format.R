6ed2727defe361627499923cbe1b4b4282da5b6b
bzkrouse <rebecca_zabel@rhoworld.com>
2018-09-04 10:42:00 -0400

    Create function for auto-formatting

---
+++ R/format.R
@@ -0,0 +1,16 @@
+#' Automatically format column values
+#'
+#' @param x A vector of values to format automatically
+#'
+#' @return A formatted vector
+#' @noRd
+#'
+#' @examples
+format_value_auto <- function(x){
+  case_when(
+    is.na(x) | x %in% c(Inf, -Inf) | is.nan(x) ~ ".",
+    x < 10 ~ format(round(x, 2), trim = TRUE, nsmall = 2),
+    x < 100 ~ format(round(x, 1), trim = TRUE, nsmall = 1),
+    TRUE ~ format(round(x, 0), trim = TRUE, nsmall = 0)
+  )
+}
