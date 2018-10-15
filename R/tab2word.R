68ec2982c9f0f9c63e7a5584c34875f406630e43
Stephanie Lussier <slussier@ms.rhoworld.com>
2018-10-15 10:10:02 -0400

    allow custom header naming

--- R/tab2word.R
+++ R/tab2word.R
@@ -21,7 +21,7 @@ tab2word <- function(table, file, footer, colwid = 1, headnames = NA){
   
   n <- 3 #this needs to be number of groups
   
-  if (is.na(headnames)){
+  if (is.na(headnames[1])){
   head <- c("sep1", "sep2", "sep3", names(table)) %>% tidy %>%
     mutate(names = c("", "", "", names(table)),
            colA = c('', '', '', names(table)[1], rep(attributes(table)$stat, n), rep(attributes(table)$comp, n), rep('p-value', n))
